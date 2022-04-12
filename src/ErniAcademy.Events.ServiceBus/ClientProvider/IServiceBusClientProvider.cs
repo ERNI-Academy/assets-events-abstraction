7ec445cfca9952e081c66b6205eb82efd1946707
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-12 15:50:31 +0200

    add service bus impl

---
+++ src/ErniAcademy.Events.ServiceBus/ClientProvider/IServiceBusClientProvider.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace ErniAcademy.Events.ServiceBus.ClientProvider;
+
+/// <summary>
+/// Contract to provide an instance of ServiceBusClient
+/// </summary>
+public interface IServiceBusClientProvider
+{
+    /// <summary>
+    /// Get ServiceBusClient
+    /// </summary>
+    /// <returns>ServiceBusClient instance</returns>
+    ServiceBusClient GetClient();
+}
---
+++ src/ErniAcademy.Events.ServiceBus/Configuration/FullyQualifiedNamespaceOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ErniAcademy.Events.ServiceBus.Configuration;
+
+public class FullyQualifiedNamespaceOptions
+{
+    /// <summary>
+    /// The fully qualified Service Bus namespace to connect to. This is likely to be similar to {yournamespace}.servicebus.windows.net.
+    /// </summary>
+    public string FullyQualifiedNamespace { get; set; }
+
+}
