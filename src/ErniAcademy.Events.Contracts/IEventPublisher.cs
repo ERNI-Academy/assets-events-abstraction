61bbf8c66465803f1a505aaacaa69ce11230f658
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-12 14:49:05 +0200

    fix @events arg and add CancellationToken support

--- src/ErniAcademy.Events.Contracts/IEventPublisher.cs
+++ src/ErniAcademy.Events.Contracts/IEventPublisher.cs
@@ -10,16 +10,18 @@ public interface IEventPublisher
     /// </summary>
     /// <typeparam name="TEvent"></typeparam>
     /// <param name="event">The event to be publish</param>
+    /// <param name="cancellationToken">An optional System.Threading.CancellationToken instance to signal the request to cancel the operation.</param>
     /// <returns>A task that represents the asynchronous publish operation</returns>
-    Task PublishAsync<TEvent>(TEvent @event) 
+    Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) 
         where TEvent : class, IEvent, new();
 
     /// <summary>
     /// Publisher of events
     /// </summary>
     /// <typeparam name="TEvent"></typeparam>
-    /// <param name="event">The events to be publish</param>
+    /// <param name="events">The events to be publish</param>
+    /// <param name="cancellationToken">An optional System.Threading.CancellationToken instance to signal the request to cancel the operation.</param>
     /// <returns>A task that represents the asynchronous publish operation</returns>
-    Task PublishAsync<TEvent>(TEvent[] @event)
+    Task PublishAsync<TEvent>(TEvent[] events, CancellationToken cancellationToken = default)
         where TEvent : class, IEvent, new();
 }
