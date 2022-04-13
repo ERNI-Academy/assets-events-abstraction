58caa4c93ecbc5b3efdd0a36e1fea0632926e654
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-13 10:22:33 +0200

    add event grid impl

---
+++ src/ErniAcademy.Events.EventGrid/ClientProvider/IEventGridPublisherClientProvider.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Messaging.EventGrid;
+
+namespace ErniAcademy.Events.EventGrid.ClientProvider;
+
+/// <summary>
+/// Contract to provide an instance of EventGridPublisherClient
+/// </summary>
+public interface IEventGridPublisherClientProvider
+{
+    /// <summary>
+    /// Get EventGridPublisherClient
+    /// </summary>
+    /// <returns>EventGridPublisherClient instance</returns>
+    EventGridPublisherClient GetClient();
+}
