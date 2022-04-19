ea99116e84e03a0dba0fa170b8f7bb269239416b
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-19 10:28:57 +0200

    fix redis tests

--- tests/ErniAcademy.Events.IntegrationTests/RedisTests.cs
+++ tests/ErniAcademy.Events.IntegrationTests/RedisTests.cs
@@ -13,12 +13,16 @@ public class RedisTests : BaseTests
 {
     private readonly ISubscriber _subscriber;
     private readonly ISerializer _serializer = new JsonSerializer();
-
+    private DummyEvent _result = default(DummyEvent);
 
     public RedisTests()
     {
         var connectionMultiplexerProvider = _provider.GetRequiredService<IConnectionMultiplexerProvider>();
         _subscriber = connectionMultiplexerProvider.Connection.GetSubscriber();
+
+        _subscriber.Subscribe(new RedisChannel("dummyevent", RedisChannel.PatternMode.Auto), (channel, value) => {
+            _result = _serializer.DeserializeFromString<DummyEvent>(value.ToString());
+        });
     }
 
     protected override IServiceCollection RegisterSut(IServiceCollection services)
@@ -29,16 +33,10 @@ public class RedisTests : BaseTests
 
     protected override async Task<DummyEvent> WaitForReceive()
     {
-        DummyEvent result = default(DummyEvent);
-
-        await _subscriber.SubscribeAsync(new RedisChannel("dummyevent", RedisChannel.PatternMode.Auto), (channel, value) => {
-            result = _serializer.DeserializeFromString<DummyEvent>(value.ToString());
-        });
-
         await Task.Delay(TimeSpan.FromSeconds(5));
 
         await _subscriber.UnsubscribeAllAsync();
 
-        return result;
+        return _result;
     }
 }
