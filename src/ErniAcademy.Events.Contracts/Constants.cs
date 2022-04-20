0d735d5802732a691d51275b932da56ba47c26ed
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-20 11:51:33 +0200

    remove not used constants

--- src/ErniAcademy.Events.Contracts/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ErniAcademy.Events.Contracts;
-
-public static class Constants
-{
-    public const string MetadataContentTypeKey = "ContentType";
-    public const string MetadataVersionKey = "Version";
-}
