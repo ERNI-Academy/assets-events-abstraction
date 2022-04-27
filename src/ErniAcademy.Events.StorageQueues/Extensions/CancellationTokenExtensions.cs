b45d892327dbf5bbbea6e25c309c1bcc0cabdbfb
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-27 13:20:32 +0200

    add CancellationTokenExtensions ThrowIfCancellationRequested

---
+++ src/ErniAcademy.Events.StorageQueues/Extensions/CancellationTokenExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace ErniAcademy.Events.StorageQueues.Extensions;
+
+/// <summary>
+///   The set of extensions for the <see cref="CancellationToken" />
+///   struct.
+/// </summary>
+internal static class CancellationTokenExtensions
+{
+    /// <summary>
+    ///   Throws an exception of the requested type if cancellation has been requested
+    ///   of the <paramref name="instance" />.
+    /// </summary>
+    /// <typeparam name="TException">The type of exception to throw; the type must have a parameterless constructor.</typeparam>
+    /// <param name="instance">The instance that this method was invoked on.</param>
+    public static void ThrowIfCancellationRequested<TException>(this CancellationToken instance) 
+        where TException : Exception, new()
+    {
+        if (instance.IsCancellationRequested)
+        {
+            throw new TException();
+        }
+    }
+}
